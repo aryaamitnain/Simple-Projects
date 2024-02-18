0db43bd2af9a1bed118b6a58aaae4904d24e003b
Amit Nain <159742510+ChAmitNain@users.noreply.github.com>
2024-02-18 12:24:38 +0000

    completed

--- Responsive NavBar/script.js
+++ Responsive NavBar/script.js
@@ -1,8 +1,8 @@
-function showsidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display= 'flex';
+function showsidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "flex";
 }
-function closesidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display= 'none';
+function closesidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = "none";
 }
